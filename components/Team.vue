c77eca23e306f76af7f62173705aaa0f3045e967
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-06 16:46:06 +0530

    Create Team component

---
+++ components/Team.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="team">
+    <h1 class="primary-heading">Team</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <h1>IET Website 2021</h1>
+    <Team />
     <Gallery id="event-gallery" />
     <ContactSection />
   </div>
 </template>
 
 <script>
+import Team from "~/components/Team.vue";
 import Gallery from "~/components/Gallery.vue";
 import ContactSection from "~/components/ContactSection.vue";
 
 export default {
   components: {
+    Team,
     Gallery,
     ContactSection,
   },
