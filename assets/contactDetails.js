a37a2a8d9cf745368f6c6412c7460766ceab144f
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-07 01:20:43 +0530

    Create contactDetails

---
+++ assets/contactDetails.js
@@ -0,0 +1,20 @@
+const contactDetails = [
+    {
+        link: "https://www.instagram.com/ietbitmesra",
+        platform: "Instagram",
+    },
+    {
+        link: "https://www.facebook.com/ietbitmesra",
+        platform: "Facebook",
+    },
+    {
+        link: "https://www.linkedin.com/company/iet-on-campus-bit-mesra",
+        platform: "Linkedin",
+    },
+    {
+        link: "https://github.com/ietbitmesra",
+        platform: "Github",
+    },
+];
+
+export default contactDetails;
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -45,27 +45,12 @@
 </template>
 
 <script>
+import contactDetails from "~/assets/contactDetails";
+
 export default {
   data() {
     return {
-      socialLinks: [
-        {
-          link: "https://www.instagram.com/ietbitmesra",
-          platform: "Instagram",
-        },
-        {
-          link: "https://www.facebook.com/ietbitmesra",
-          platform: "Facebook",
-        },
-        {
-          link: "https://www.linkedin.com/company/iet-on-campus-bit-mesra",
-          platform: "Linkedin",
-        },
-        {
-          link: "https://github.com/ietbitmesra",
-          platform: "Github",
-        },
-      ],
+      socialLinks: contactDetails,
     };
   },
 };
