c2c6755c44948b701f9cf4066317f4313a59816c
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-06 14:28:52 +0530

    Create Gallery component

---
+++ components/Gallery.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="gallery">
+    <h1 class="primary-heading">Gallery</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <h1>IET Website 2021</h1>
+    <Gallery />
     <ContactSection />
   </div>
 </template>
 
 <script>
+import Gallery from "~/components/Gallery.vue";
 import ContactSection from "~/components/ContactSection.vue";
 
 export default {
   components: {
+    Gallery,
     ContactSection,
   },
 };
