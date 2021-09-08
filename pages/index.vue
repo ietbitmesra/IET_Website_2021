d6e4891f3c8057fb2aa276350a3eca4a8a70f66d
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-08 13:48:18 +0530

    Create workshops component

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>IET Website 2021</h1>
     <Team />
+    <Workshops />
     <Gallery id="event-gallery" />
     <ContactSection />
   </div>
@@ -9,12 +10,14 @@
 
 <script>
 import Team from "~/components/Team.vue";
+import Workshops from "~/components/Workshops.vue";
 import Gallery from "~/components/Gallery.vue";
 import ContactSection from "~/components/ContactSection.vue";
 
 export default {
   components: {
     Team,
+    Workshops,
     Gallery,
     ContactSection,
   },
