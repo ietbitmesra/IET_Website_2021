3f6ba3cebe1e90cb3ed068f10583994476f479e8
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-09 00:35:46 +0530

    Create HackABIT section

---
+++ components/HackABIT.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hack-a-bit-section">
+    <img
+      src="https://images.unsplash.com/photo-1472145246862-b24cf25c4a36?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHB1cnBsZXxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      alt="Hack-A-BIT"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
