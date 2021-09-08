735f272936bf487dd72fba8680df8561f99868a0
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-09 01:16:55 +0530

    Add content and styling

--- components/HackABIT.vue
+++ components/HackABIT.vue
@@ -1,9 +1,33 @@
 <template>
   <div class="hack-a-bit-section">
-    <img
-      src="https://images.unsplash.com/photo-1472145246862-b24cf25c4a36?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHB1cnBsZXxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-      alt="Hack-A-BIT"
-    />
+    <div class="poster">
+      <img
+        class="event-poster"
+        src="https://images.unsplash.com/photo-1472145246862-b24cf25c4a36?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHB1cnBsZXxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        alt="Hack-A-BIT"
+      />
+    </div>
+    <div class="event-details">
+      <h1 class="primary-heading">Hack-A-BIT</h1>
+      <p class="about-event">
+        Hack-A-BIT has turned out to be one of the
+        <b>Largest Student Organised Hackathons</b> in the country. The maiden
+        edition of this venture saw great enthusiasm from both the industry and
+        academia, and is set to grow bigger and bigger as the years progress.
+        IET, on campus BIT Mesra is one of the Clubs involved in its
+        organisation. Letting new ideas flow through young minds, and
+        challenging students to build the next big thing is the prime focus of
+        this endeavour.
+      </p>
+      <a
+        class="website-btn"
+        href="https://www.hackabit.in"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button class="btn secondary-btn">Hack-A-BIT Website</button>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -12,4 +36,49 @@ export default {};
 </script>
 
 <style scoped>
+.hack-a-bit-section {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 100vh;
+}
+.poster {
+  flex: 1;
+  margin-right: 2rem;
+}
+.event-poster {
+  max-width: 530px;
+  width: 100%;
+}
+.event-details {
+  flex: 1;
+  margin-left: 2rem;
+}
+.about-event {
+  color: #656565;
+  font-size: 0.9rem;
+  font-weight: 400;
+  margin-bottom: 2rem;
+  text-align: justify;
+}
+@media only screen and (max-width: 1220px) {
+  .hack-a-bit-section {
+    margin: 0 1rem;
+  }
+}
+@media only screen and (max-width: 1080px) {
+  .hack-a-bit-section {
+    flex-direction: column;
+  }
+  .poster {
+    flex: unset;
+    margin: 2rem 0;
+  }
+  .event-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-left: 0;
+  }
+}
 </style>
