d33eeed6e16d716aea5040a0a872838b37b4c119
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-29 18:42:43 +0530

    Add content and style

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="hero-section" id="home">
-    <h1>home</h1>
+    <div class="cta-div">
+      <IETHomeSvg />
+      <h2 class="tagline">Learn. Grow. Excel</h2>
+    </div>
+    <div class="icons-div">
+      <IconTilesSvg />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import IETHomeSvg from "~/components/IETHomeSvg.vue";
+import IconTilesSvg from "~/components/IconTilesSvg.vue";
+
+export default {
+  components: {
+    IETHomeSvg,
+    IconTilesSvg,
+  },
+};
 </script>
 
 <style scoped>
+.hero-section {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 100vh;
+  margin: auto;
+  max-width: 1300px;
+}
+.tagline {
+  color: #adadad;
+  font-weight: 300;
+  letter-spacing: 10px;
+  text-align: center;
+  text-transform: uppercase;
+}
 </style>
