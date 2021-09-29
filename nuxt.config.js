baa897f16192447951a55f3a7c12fbed478dbe65
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-30 01:42:34 +0530

    Change font

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
       },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Poppins:wght@400;500;700&display=swap"
+        href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
       }
     ]
   },
