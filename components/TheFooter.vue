85cf192749d3e5b69d9fe55e88fbe65ebbfc0fe1
Ayush Acharya <ayushjash1323@gmail.com>
2021-09-30 15:45:48 +0530

    Add navigation functionality

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -9,13 +9,13 @@
       </div>
       <div class="site-map">
         <h3 class="section-heading">Sitemap</h3>
-        <p class="link">Home</p>
-        <p class="link">About</p>
-        <p class="link">Workshops</p>
-        <p class="link">Events</p>
-        <p class="link">Gallery</p>
-        <p class="link">Team</p>
-        <p class="link">Contact</p>
+        <p class="link" @click="scrollToTop">Home</p>
+        <p class="link" @click="scrollToSection('about')">About</p>
+        <p class="link" @click="scrollToSection('workshops')">Workshops</p>
+        <p class="link" @click="scrollToSection('events')">Events</p>
+        <p class="link" @click="scrollToSection('gallery')">Gallery</p>
+        <p class="link" @click="scrollToSection('team')">Team</p>
+        <p class="link" @click="scrollToSection('contact')">Contact</p>
       </div>
       <div class="socials-section">
         <h3 class="section-heading">Social Links</h3>
@@ -48,12 +48,17 @@
 
 <script>
 import contactDetails from "~/assets/contactDetails";
+import { scrollToTop, scrollToSection } from "~/assets/methods";
 
 export default {
   data() {
     return {
       socialLinks: contactDetails,
     };
+  },
+  methods: {
+    scrollToTop: () => scrollToTop(),
+    scrollToSection: (id) => scrollToSection(id),
   },
 };
 </script>
