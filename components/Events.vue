67940c4745e7367febfd15e50d45de9d3ad463b8
pseudobot <shivamx6174@gmail.com>
2021-09-30 19:15:00 +0530

    Hover effect on event posters

--- components/Events.vue
+++ components/Events.vue
@@ -3,24 +3,99 @@
     <h1 class="events-heading primary-heading">Events</h1>
     <div class="events-gallery">
       <div class="column">
-        <img class="event-poster" src="~/assets/events/month_of_code.jpeg" />
-        <img class="event-poster" src="~/assets/events/brainfookers.jpg" />
-        <img class="event-poster" src="~/assets/events/captureTheFlag.jpg" />
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/month_of_code.jpeg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/brainfookers.jpg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/captureTheFlag.jpg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
       </div>
       <div class="column">
-        <img class="event-poster" src="~/assets/events/codec.jpg" />
-        <img class="event-poster" src="~/assets/events/codeItBlind.jpg" />
-        <img class="event-poster" src="~/assets/events/dcrypt.jpeg" />
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/codec.jpg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/codeItBlind.jpg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/dcrypt.jpeg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
       </div>
       <div class="column">
-        <img class="event-poster" src="~/assets/events/githeat.jpg" />
-        <img class="event-poster" src="~/assets/events/google_it_out.jpg" />
-        <img class="event-poster" src="~/assets/events/techdesk.jpg" />
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/githeat.jpg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/google_it_out.jpg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/techdesk.jpg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
       </div>
       <div class="column">
-        <img class="event-poster" src="~/assets/events/ultimate_engineer.jpg" />
-        <img class="event-poster" src="~/assets/events/walking_dead.jpg" />
-        <img class="event-poster" src="~/assets/events/blitzkreig.jpg" />
+        <div class="event-card">
+          <img
+            class="event-poster"
+            src="~/assets/events/ultimate_engineer.jpg"
+          />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/walking_dead.jpg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
+        <div class="event-card">
+          <img class="event-poster" src="~/assets/events/blitzkreig.jpg" />
+          <div class="event-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            dolor dolores quaerat nobis optio ea alias error eum unde fugiat.
+          </div>
+        </div>
       </div>
     </div>
     <HackABIT />
@@ -59,6 +134,36 @@ export default {};
   vertical-align: middle;
   width: 100%;
 }
+
+.event-card {
+  position: relative;
+  overflow: hidden;
+  border-radius: 5px;
+}
+
+.event-poster {
+  cursor: pointer;
+  transition: filter 0.6s;
+  border-radius: 5px;
+}
+.event-description {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  padding: 1.25rem;
+  text-align: justify;
+  opacity: 0;
+  transition: opacity 0.6s;
+  cursor: pointer;
+  color: #fff;
+}
+.event-card:hover .event-poster {
+  filter: blur(.75rem) brightness(70%);
+}
+.event-card:hover .event-description {
+  opacity: 1;
+}
+
 @media screen and (max-width: 800px) {
   .column {
     -ms-flex: 50%;
