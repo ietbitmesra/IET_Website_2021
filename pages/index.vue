3639ee7a343f9719318270bd6b313b73d96f2871
Ayush Acharya <ayushjash1323@gmail.com>
2021-10-05 18:18:35 +0530

    Update section ordering

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
   <div>
     <HeroSection />
     <AboutSection />
-    <Team />
-    <Workshops />
     <Events />
+    <Workshops />
+    <Team />
     <ContactSection />
   </div>
 </template>
@@ -12,18 +12,18 @@
 <script>
 import HeroSection from "~/components/HeroSection.vue";
 import AboutSection from "~/components/AboutSection.vue";
-import Team from "~/components/Team.vue";
-import Workshops from "~/components/Workshops.vue";
 import Events from "~/components/Events.vue";
+import Workshops from "~/components/Workshops.vue";
+import Team from "~/components/Team.vue";
 import ContactSection from "~/components/ContactSection.vue";
 
 export default {
   components: {
     HeroSection,
     AboutSection,
-    Team,
-    Workshops,
     Events,
+    Workshops,
+    Team,
     ContactSection,
   },
 };
