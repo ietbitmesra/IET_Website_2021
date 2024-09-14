46fd687b5ac5e763e9455fcf218478250da8f862
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-09-14 13:24:53 +0530

    members updated

---
+++ assets/teamDetails/team_2024.js
@@ -0,0 +1,120 @@
+const team_2024 = {
+    year: "2024 - 2025",
+    members: [
+        [
+            {
+                "name": "Pratham Agarwal",
+                "position": "President",
+                "imagePath": "https://drive.google.com/open?id=1LjHrkF9lGOL3t7-Eqv_UzhUPi_uRMUeZ",
+                "linkedinLink": "https://www.linkedin.com/in/pratham-agarwal-9a4505223/"
+            },
+            {
+                "name": "Sumit Agarwal",
+                "position": "Vice President",
+                "imagePath": "https://drive.google.com/open?id=1yt--tP3le09xEo6FPM1RONdAhhU5XL7S",
+                "linkedinLink": "https://www.linkedin.com/in/sumit-agarwal2002/"
+            },
+            {
+                "name": "Khushi Kumari",
+                "position": "Vice President",
+                "imagePath": "https://drive.google.com/open?id=13kUaVKm2ScMYqYzQEbLSGyc1Q35zyrpt",
+                "linkedinLink": "https://www.linkedin.com/in/khushi-kumari-7007491a0/"
+            },
+            {
+                "name": "Ayush Kumar",
+                "position": "Director",
+                "imagePath": "https://drive.google.com/open?id=1kLFTZsWeym2C8I2RSPBUad5jn8fdQJzD",
+                "linkedinLink": "https://www.linkedin.com/in/ayushkumar3070/"
+            },
+            {
+                "name": "Amrit Yashasvi Lal",
+                "position": "Director",
+                "imagePath": "https://drive.google.com/open?id=1XsBJs856GCP4wQ5D1TeecrrsyPgBwqgi",
+                "linkedinLink": "https://www.linkedin.com/in/amrit-yashasvi-lal-604672232?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+            },
+            {
+                "name": "Gajendra Agarwal",
+                "position": "Senior Executive Member",
+                "imagePath": "https://drive.google.com/open?id=1J3gz5arpHznCDavYvRCdKfkXnZ0F5jNu",
+                "linkedinLink": "https://www.linkedin.com/in/gajendra-agarwal-a51aa9228/"
+            },
+            // priyanshu singhal 
+            {
+                "name": "Anish Kumar Singh",
+                "position": "Senior Executive Member",
+                "imagePath": "https://drive.google.com/open?id=178KiSwycHTTPC3eVLa4S1_RMDzICIcsq",
+                "linkedinLink": "https://linkedin.com/in/anish-kumar-singh-155334222"
+            },
+            // ayush raturi
+            {
+                "name": "Harsh Raj",
+                "position": "Senior Executive Member",
+                "imagePath": "https://drive.google.com/open?id=1XhDXISJkIlaVjZoOn07lPjBXbRJxqwSc",
+                "linkedinLink": "https://www.linkedin.com/in/harsh-raj-133893226/"
+            },
+            // prateek khandelwal
+            // dhruv mittal
+            {
+                "name": "Nayan Shenoy",
+                "position": "General Secretary",
+                "imagePath": "https://drive.google.com/open?id=1f1XdPpqN1Z6pi6M-m2_FBPKV05xkZGjB"
+            },
+            {
+                "name": "Keshav Dutt Gautam",
+                "position": "General Secretary",
+                "imagePath": "https://drive.google.com/open?id=1VhZHiL_rlQzHLUHKDsJGO31USkfWQE6R",
+                "linkedinLink": "https://www.linkedin.com/in/keshav-dutt-gautam-71469b273?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BWUFp4Yj%2FQTKEgoxJt3uSgA%3D%3D"
+            },
+            // rohit dalai
+            {
+                "name": "Abhijeet Kumar",
+                "position": "Joint Secretary",
+                "imagePath": "https://drive.google.com/open?id=1pFjq2aWiIYecjDqgc9qw845P2wUkco3S",
+                "linkedinLink": "https://www.linkedin.com/in/abhijeet-kumar-b99770249?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+            },
+            {
+                "name": "Aditya Srivastava",
+                "position": "Treasurer",
+                "imagePath": "https://drive.google.com/open?id=1EuF9kjqR9CzqHGbcdwcvcF10DTnaF6DZ",
+                "linkedinLink": "https://www.linkedin.com/in/aditya-srivastava-12476524a/"
+            },
+            {
+                "name": "Kushagra Sahay",
+                "position": "Web Master",
+                "imagePath": "https://drive.google.com/open?id=1nazMPRoaICLJmFenAdOp6WQHKaL5eDku",
+                "linkedinLink": "https://www.linkedin.com/in/kushagra-sahay-500b671b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+            },
+            {
+                "name": "VARUN GUPTA",
+                "position": "Public Relation Co-ordinator",
+                "imagePath": "https://drive.google.com/open?id=1VhQUshzH7ll00fjZBJdCs8het9EUFSfO",
+                "linkedinLink": "https://www.linkedin.com/in/ivarung"
+            },
+            {
+                "name": "Shreyansh Gupta",
+                "position": "Public Relation Co-ordinator",
+                "imagePath": "https://drive.google.com/open?id=1Vnx4OoW022TsPh8pXMaXrwaeCQkhKy3d",
+                "linkedinLink": "https://www.linkedin.com/in/shreyansh-gupta-043b4b25b"
+            },
+            {
+                "name": "Akshat Sahay",
+                "position": "DESIGN HEAD",
+                "imagePath": "https://drive.google.com/open?id=1u997EoHSKJa2XEHtQCyPubozoI1v0qya",
+                "linkedinLink": "https://www.linkedin.com/in/akshat-sahay-6263b9268/"
+            },
+            {
+                "name": "Soumojjal Sen",
+                "position": "Tech Lead",
+                "imagePath": "https://drive.google.com/open?id=1FxxuqsYJAuPtWqEvHAdzS8fezWDyAV7O",
+                "linkedinLink": "https://www.linkedin.com/in/soumojjal-sen-7b8490256"
+            },
+            {
+                "name": "Anand Raj",
+                "position": "Tech Lead",
+                "imagePath": "https://drive.google.com/open?id=13D8j8EIxpxb2sHXFQ4Xrk9abnsMQpDtb",
+                "linkedinLink": "https://www.linkedin.com/in/anand-raj-10449524b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+            }
+        ]        
+    ]
+};
+export default team_2024;
--- pages/previous-teams/index.vue
+++ pages/previous-teams/index.vue
@@ -2,6 +2,10 @@
   <div class="previous-teams">
     <h1 class="primary-heading">Previous Teams</h1>
     <div class="team-members">
+      <p class="year">2024 - 2025</p>
+      <TeamMemberCard v-for="member_2024 in teamMembers_2024" :key="member_2024.name" :memberDetails="member_2024" />
+    </div>
+    <div class="team-members">
       <p class="year">2022 - 2023</p>
       <TeamMemberCard v-for="member_2022 in teamMembers_2022" :key="member_2022.name" :memberDetails="member_2022" />
     </div>
@@ -26,6 +30,7 @@
 
 <script>
 import TeamMemberCard from "~/components/TeamMemberCard.vue";
+import team_2024 from "~/assets/teamDetails/team_2024";
 import team_2022 from "~/assets/teamDetails/team_2022";
 import team_2021 from "~/assets/teamDetails/team_2021";
 import team_2020 from "~/assets/teamDetails/team_2020";
@@ -34,6 +39,7 @@ export default {
   layout: "secondary",
   data() {
     return {
+      teamMembers_2024: team_2024.members,
       teamMembers_2022: team_2022.members,
       teamMembers_2021: team_2021.members,
       teamMembers_2020: team_2020.members,
